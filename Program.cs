39b326d1ddc6f91801bda890d0ef2eb38b68d3ce
Ibrohim <irgashev1261@gmail.com>
2023-08-22 13:58:18 +0500

    IMPLEMENT: 7th home work, add validation

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 ﻿using crmProject.Dtos;
 using crmProject.Services;
 
-CreateClient();
+Console.WriteLine("Выбериите действие: Создать клиента или Создать заказ");
+string command = Console.ReadLine();
+
+switch (command)
+{
+    case "Создать клиента":
+        CreateClient();
+        break;
+    case "Создать заказ":
+        CreateOrder();
+        break;
+    default:
+        Console.WriteLine("Неизвестная команда");
+        break;
+}
 
 void CreateClient()
 {
@@ -33,6 +47,8 @@ void CreateClient()
         Gender = gender
     };
 
+    if (!ValidateClient(clientDto)) return;
+
     ClientService.CreateClient(clientDto);
 }
 
@@ -40,27 +56,95 @@ void CreateOrder()
 {
     Console.WriteLine("Введите описание заказа: ");
     string description = Console.ReadLine();
-    
+
     Console.WriteLine("Введите итоговую сумму заказа: ");
     string price = Console.ReadLine();
-    
+
     Console.WriteLine("Введите дату доставки заказа: ");
     string date = Console.ReadLine();
-    
+
     Console.WriteLine("Введите тип доставки: ");
     string deliveryType = Console.ReadLine();
-    
+
     Console.WriteLine("Введите адресс доставки: ");
     string deliveryAddress = Console.ReadLine();
 
     OrderDto orderDto = new OrderDto()
     {
-        Description =description,
+        Description = description,
         Price = price,
         Date = date,
         DeliveryType = deliveryType,
         DeliveryAddress = deliveryAddress
     };
 
+    if (!ValidateOrder(orderDto)) return;
+
+
     OrderService.CreateOrder(orderDto);
+}
+
+bool ValidateClient(ClientDto clientDto)
+{
+    List<string> errors = new();
+
+    if (clientDto.Name is { Length: 0 })
+        errors.Add("Name field is required!");
+
+    if (clientDto.Surname is { Length: 0 })
+        errors.Add("Surname field is required");
+
+    if (clientDto.Patronymic is { Length: 0 })
+        errors.Add("Patronymic field is required");
+
+    bool isAgeCorrect = ushort.TryParse(clientDto.Age, out ushort age);
+    if (!isAgeCorrect)
+        errors.Add("Please input correct value for age field!");
+
+    if (clientDto.PassportNumber is { Length: 0 })
+        errors.Add("Passport number field is required");
+
+    if (clientDto.Gender != "male" && clientDto.Gender != "female")
+        errors.Add("Please input correct value for gender field (male, female)!");
+
+    if (errors is not { Count: > 0 }) return true;
+    foreach (string error in errors)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(error);
+    }
+
+    Console.ForegroundColor = ConsoleColor.White;
+    return false;
+}
+
+bool ValidateOrder(OrderDto orderDto)
+{
+    List<string> errors = new();
+
+    if (orderDto.Description is { Length: 0 })
+        errors.Add("Заполните описание для вашего заказа");
+
+    bool isPriceCorrect = double.TryParse(orderDto.Price, out double price);
+    if (!isPriceCorrect)
+        errors.Add("Введите правильное значение для поля цена");
+
+    if (orderDto.Date is { Length: 0 })
+        errors.Add("Заполните дату для вашего заказа");
+
+    if (orderDto.DeliveryType != "express" && orderDto.DeliveryType != "standard" && orderDto.DeliveryType != "free")
+        errors.Add("Выберите правильный тип доставки");
+
+    if (orderDto.DeliveryAddress is { Length: 0 })
+        errors.Add("Заполните адрес доставки для вашего заказа");
+
+    if (errors is not { Count: > 0 }) return true;
+    foreach (string error in errors)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(error);
+    }
+
+    Console.ForegroundColor = ConsoleColor.White;
+    return false;
 }
